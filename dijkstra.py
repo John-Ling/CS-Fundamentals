73c1e493a07951170cd536612462ac15b8c10539
John Ling <johnlingbusiness@gmail.com>
2023-02-09 22:41:03 +0800

    Refactoring

--- dijkstra.py
+++ dijkstra.py
@@ -1,13 +1,6 @@
-import heapq
-
 SOURCE_NODE = 'A'
 
 def main():
-	# GRAPH = {'A': [(6, 'B'), (1, 'D')],
-	# 		 'B': [(5, 'C'), (2, 'D'), (2, 'E')],
-	# 		 'C': [(5, 'B'), (5, 'E')],
-	# 		 'D': [(1, 'A'), (2, 'B'), (1, 'E')],
-	# 		 'E': [(2, 'B'), (1, 'D'), (5, 'C')]}
 	GRAPH = {'A': [(5, 'B'), (6, 'C')],
 		    'B': [(5, 'A'), (4, 'D'), (6, 'E')],
 		    'C': [(6, 'A'), (7, 'E'), (6, 'F')],
@@ -27,20 +20,15 @@ def generate_shortest_paths(GRAPH, source):
 	# 1. Update the shortest distances between adjacent nodes
 	# 2. Choose the next vertex based on the shortest distance
 
-	distances = {}  # the distance table for all nodes from the source node
-	shortestPaths = {} 
+	distances = {}  # the distance table for all nodes from the source node 
 	previousNodes = {}
-	nodeQueue = [] # heap to act as a priority queue for nodes
 	unexploredNodes = list(GRAPH.keys()) # all nodes start as unvisited
 
 	# set the shortest distance as infinity for all vertices 
 	for node in unexploredNodes:
 		distances[node] = float('inf')
-		shortestPaths[node] = []
 	currentNode = source
-
 	distances[source] = 0 # we know the shortest distance from source to itself is 0
-	del shortestPaths[source] # we already know the shortest path to source because its itself
 
 	while len(unexploredNodes) != 0: # explore every node
 		for i in range(len(GRAPH[currentNode])): # check adjacent nodes to the current node
@@ -61,36 +49,6 @@ def generate_shortest_paths(GRAPH, source):
 		currentNode = nextNode
 	
 	return (distances, previousNodes)
-
-def dijkstras_heap(G, start='A'):
-    shortest_paths = {} 
-    visited = {} 
-    history = {} 
-    heap = [] 
-    path = []
-
-    for node in list(G.keys()):
-        shortest_paths[node] = float('inf')
-        visited[node] = False
-
-    shortest_paths[start] = 0 
-    visited[start] = True
-
-    heapq.heappush(heap, (0, start))
-
-    while heap:
-        (distance, node) = heapq.heappop(heap)
-        visited[node] = True
-
-        for edge in G[node]:
-            cost = edge[0]
-            to_node = edge[1]
-
-            if (not visited[to_node]) and (distance + cost < shortest_paths[to_node]):
-                shortest_paths[to_node] = distance + cost
-                heapq.heappush(heap, (shortest_paths[to_node], to_node))
-
-    return shortest_paths
 
 def get_shortest_path(destination, graphData):
 	# given a destination and graph data created by dijkstra's algorithm, generate a list of nodes to travel and the total distance / cost to reach destination from the source node

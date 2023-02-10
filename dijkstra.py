0d8cfd4bb0831a1455df5b5e289cc231e138e65d
John Ling <johnlingbusiness@gmail.com>
2023-02-10 09:14:16 +0800

    Terminology change

--- dijkstra.py
+++ dijkstra.py
@@ -14,59 +14,51 @@ def main():
 	print(f"Shortest path distance: {data[1]}")
 
 def generate_shortest_paths(GRAPH, source): 
-	# generate the shortest paths between source vertex and every other node in the graph
-
-	# key steps in dijkstra's algorithm
-	# 1. Update the shortest distances between adjacent nodes
-	# 2. Choose the next vertex based on the shortest distance
-
-	distances = {}  # the distance table for all nodes from the source node 
+	costs = {}  # the cost table for all nodes from the source node 
 	previousNodes = {}
-	unexploredNodes = list(GRAPH.keys()) # all nodes start as unvisited
+	unexploredNodes = list(GRAPH.keys())
 
-	# set the shortest distance as infinity for all vertices 
+	# set the cost as infinity for all vertices (we don't know the initial costs yet)
 	for node in unexploredNodes:
-		distances[node] = float('inf')
+		costs[node] = float('inf')
 	currentNode = source
-	distances[source] = 0 # we know the shortest distance from source to itself is 0
+	costs[source] = 0 # we know the shortest distance from source to itself is 0
 
-	while len(unexploredNodes) != 0: # explore every node
-		for i in range(len(GRAPH[currentNode])): # check adjacent nodes to the current node
-			node = GRAPH[currentNode][i][1] # the adjacent node connected to the current node
-			distance = GRAPH[currentNode][i][0] + distances[currentNode]; # distance is distance of the current node + the distance to the adjacent node
+	while len(unexploredNodes) != 0: 
+		for i in range(len(GRAPH[currentNode])):
+			node = GRAPH[currentNode][i][1] 
+			cost = GRAPH[currentNode][i][0] + costs[currentNode]; 
 
-			if distance < distances[node]: # update the shortest distance for the adjacent node
-				distances[node] = distance 
+			if cost < costs[node]: # update the cost for the adjacent node
+				costs[node] = cost
 				previousNodes[node] = currentNode
 
 		unexploredNodes.remove(currentNode) # remove the current node as we will no longer visit it
-		nextNode = ''
-		lowestDistance = float('inf')
-		for node in unexploredNodes: # select shortest unexplored node
-			if distances[node] < lowestDistance:
-				nextNode = node
-				lowestDistance = distances[node]
-		currentNode = nextNode
+		lowestCost = float('inf')
+		for node in unexploredNodes: # update current node and select lowest cost unexplored node
+			if costs[node] < lowestCost:
+				currentNode = node 
+				lowestCost = costs[node]
 	
-	return (distances, previousNodes)
+	return (costs, previousNodes)
 
 def get_shortest_path(destination, graphData):
 	# given a destination and graph data created by dijkstra's algorithm, generate a list of nodes to travel and the total distance / cost to reach destination from the source node
-	shortestDistances = graphData[0]
+	lowestCosts = graphData[0]
 	previousNodes = graphData[1]
 
-	if any(node not in shortestDistances for node in (SOURCE_NODE, destination)):
+	if any(node not in lowestCosts for node in (SOURCE_NODE, destination)):
 		print(f"Node does not exist")
 		return;
 	
-	distance = shortestDistances[destination]
+	cost = lowestCosts[destination]
 	path = [destination]
 
 	node = destination
 	while node != SOURCE_NODE:
 		path.insert(0, previousNodes[node])
 		node = previousNodes[node]
-	return (path, distance)
+	return (path, cost)
 
 if __name__ == "__main__":
 	main()

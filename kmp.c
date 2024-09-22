e46971c89ed2fb5bc993303afc48c3ef98403576
John <johnlingbusiness@gmail.com>
2024-09-23 01:31:25 +1000

    Completed kmp

--- kmp.c
+++ kmp.c
@@ -2,32 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
-int create_failure_array(int* lps, const char* pattern, const int length);
+int create_failure_array(int lps[], const char* pattern, const int length);
+int kmp_search(const char* pattern, const char* string);
+
 int main(int argc, char* argv[])
 {
-    char* pattern = "ABCDABD";
-    const int length = strlen(pattern);
-    int* lps = (int*)malloc(sizeof(int) * length);
-    create_failure_array(lps, pattern, length);
-
-    for (int i = 0; i < length; i++)
-    {
-        printf("%d ", lps[i]);
-    }
-    printf("\n");
-    free(lps);
+    const char* pattern = "ABCDABD";
+    const char* string = "ABC ABCDAB ABCDABCDABDE";
+    kmp_search(pattern, string);
     return 0;
 }
 
 int kmp_search(const char* pattern, const char* string)
 {
+    const int patternLength = strlen(pattern);
+    const int stringLength = strlen(string);
+    int lps[patternLength];
+    create_failure_array(lps, pattern, patternLength);
+
+    int i = 0;
+    int found = 1;
+
+    int pointerA = i;
+    int pointerB = patternLength;
+    while (i < stringLength)
+    {
+        int k = 0;
+        for (int j = pointerA; j < pointerB; j++)
+        {
+            if (string[j] == pattern[k])
+            {
+                k++;
+                continue;
+            }
+            found = 0;
+            i = j - lps[k] - 1; // skip over by setting index
+            break;
+        }
+
+        if (found)
+        {
+            printf("Found pattern at index %d\n", i);
+        }
+
+        found = 1;
+        pointerA++;
+        pointerB++;
+        i++;
+    }
     return 0;
 }
 
 // I'll be honest I don't fully understand how this works
 // I just took the code from geeksfromgeeks, ran it through claude and adjusted a bit
 // general idea still applies just a abstract way of implementing it I guess
-int create_failure_array(int* lps, const char* pattern, const int length)
+int create_failure_array(int lps[], const char* pattern, const int length)
 {
     if (length <= 0)
         return 1;
@@ -38,7 +67,6 @@ int create_failure_array(int* lps, const char* pattern, const int length)
     int j = -1; // length of longest prefix suffix
     while (i < length - 1)
     {
-        printf("i %d j %d\n", i, j);
         if (j == -1 || pattern[i] == pattern[j])
         {
             // either initialise value at index i as 0

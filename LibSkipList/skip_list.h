020e40bb45e03aa05560bad44c63ae5de35d9281
John Ling <johnlingbusiness@gmail.com>
2025-08-02 11:09:26 +1000

    feat: Started work on skip list

--- LibSkipList/skip_list.h
+++ LibSkipList/skip_list.h
@@ -1,11 +1,26 @@
+#ifndef SkipList_H
+#define SkipList_H
+
 #include <stdlib.h>
 #include "linked_lists.h"
+#include "utils.h"
 
+
+// try make below a fixed array for better cache locality
 typedef struct SkipListNode_t {
     void* value;
-    struct SkipListNode_t* next;
+    ListNode* next;
+    struct SkipListNode_t* below;
 } SkipListNode;
 
 typedef struct SkipList_t {
-    
+    int layerCount; // keep a maximum of 16
+    int nodeCount;  // number of nodes in the bottom layer
+    SkipListNode* head; // head of skip list
+    size_t dataSize;
 } SkipList;
+
+SkipList* sl_create(void* values[], size_t n, size_t dataSize);
+void sl_insert(SkipList* skiplist, const void* value, )
+
+#endif
